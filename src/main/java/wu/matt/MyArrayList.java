3702d14077b6ec76903ac820bf69e9a2c2484992
Matt Wu <mingzhi.wu12@gmail.com>
2017-05-18 19:30:46 -0400

    Initial Commit

---
+++ src/main/java/wu/matt/MyArrayList.java
@@ -0,0 +1,7 @@
+package wu.matt;
+
+public class MyArrayList
+{
+
+
+}
---
+++ src/test/java/wu/matt/MyArrayListTest.java
@@ -0,0 +1,13 @@
+package wu.matt;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class MyArrayListTest{
+
+
+}
